0e1c24cd70cdf624f268ef27c8250773a0b618e7
GitGolik <goliktheo.pro@gmail.com>
2026-06-23 14:08:27 +0200

    5-print_numbers.c 1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '10'; ++a)
+		puts(a);
+	putchar('\n');
+	return (0);
+}
