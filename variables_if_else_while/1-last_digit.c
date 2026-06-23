6609dc245e5007dd32d2e947713c48f06472791b
GitGolik <goliktheo.pro@gmail.com>
2026-06-23 12:53:32 +0200

    1-last_digit.c 3

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,18 +9,18 @@
  */
 int main(void)
 {
-	int n;
-	int r;
+	int n, r;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	r = n % 10;
+
 	if (r > 5)
-		printf("%i is greater than 5\n", r);
+		printf("Last digit of %i is %i and is greater than 5\n", n, r);
 	else if (r == 0)
-		printf("%i is 0\n", r);
+		printf("Last digit of %i is %i and is 0\n", n, r);
 	else if (r < 6 && r != 0)
-		printf("%i is less than 6 and not 0\n", r);
+		printf("%i is less than 6 and not 0\n", n, r);
 
 	return (0);
 }
