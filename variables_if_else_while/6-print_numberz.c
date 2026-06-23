a588e575b81f78c55cf48f29fb00411366753a11
GitGolik <goliktheo.pro@gmail.com>
2026-06-23 14:36:15 +0200

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; ++a)
+		putchar("%c", a);
+	putchar('\n');
+	return (0);
+}
