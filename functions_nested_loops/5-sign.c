2c129e920b7cfae5c09669ae20b9922f54f4641c
GitGolik <goliktheo.pro@gmail.com>
2026-06-23 22:39:39 +0200

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code
+ *
+ * @c: character
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		if (c == 0)
+			_putchar(0);
+		else
+			_putchar('-');
+	else 
+	{
+	}
+	}
+	return (0);
+}
+	
