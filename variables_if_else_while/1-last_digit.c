ccdd802b8ba243819807ec24400326757f59c82f
GitGolik <goliktheo.pro@gmail.com>
2026-06-23 12:11:56 +0200

    1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	r = n % 10;
+	if (r > 5)
+		printf("%i is greater than 5\n", r);
+	else if (r == 0)
+		printf("%i is 0\n", r);
+	else if (r < 6 && r != 0)
+		printf("%i is less than 6 and not 0\n", r);
+
+	return (0);
+}
