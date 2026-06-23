d23ecc98a4fed1e6605b96ed3f6b52f8a525b3c3
GitGolik <goliktheo.pro@gmail.com>
2026-06-23 15:05:21 +0200

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9'; ++a)
+	for (a = '0'; a <= '9'; a++)
 	{
+		putchar(a);
 		putchar(',');
 		putchar(' ');
-		putchar(a);
 	}
 	putchar('\n');
 	return (0);
