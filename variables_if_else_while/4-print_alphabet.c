b8978aecbb7d9673093a87dcdfe1a6d06a8b0a8b
GitGolik <goliktheo.pro@gmail.com>
2026-06-23 13:27:11 +0200

    4-print_alphabet.c 1

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c; c != 'q'; c != 'e')
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
