e4a90b6bc72d087ee5f1109e190a45ac7ca41b25
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 16:06:38 +0200

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ *
+ * @a: character
+ *
+ * @b: character
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	_putchar(a * b);
+	return (0);
+}
