6b110922dd5cd6d27a821f4e9324990a531cf0a7
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 16:43:45 +0200

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; ++a)
+	{
+		if ((a == '2') && (a == '4'))
+		{
+		}
+		else
+			_putchar(a);
+	}
+	_putchar('\n');
+}
