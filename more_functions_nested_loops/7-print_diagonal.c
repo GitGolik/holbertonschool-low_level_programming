32046bceaa6843317b0ba54bf3abb71e0cf0396b
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 19:51:52 +0200

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,7 +11,7 @@ void print_diagonal(int n)
 {
 	int a, b;
 
-	if (n != 0)
+	if (n > 0)
 	{
 		for (a = 0; a < n; ++a)
 		{
@@ -24,5 +24,7 @@ void print_diagonal(int n)
 		}
 	}
 	else
+	{
 		_putchar('\n');
+	}
 }
