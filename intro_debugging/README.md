30aaa6c0cbc0887f7e21b57fd6c76080a91e7a0f
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 11:50:35 +0200

    sum_to_n.c

---
+++ intro_debugging/README.md
@@ -0,0 +1,3 @@
+#README
+
+introduction to debugging your code step by step with python tutor on C 
---
+++ intro_debugging/sum_to_n.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * sum_to_n - check the code
+ *
+ * main - check the code
+ *
+ * @n: character
+ *
+ * Return: Always 0 (Success)
+ */
+int sum_to_n(int n)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < n; i++)
+		sum += i;
+
+	return n;
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+	printf("%d\n", sum_to_n(10));
+	return 0;
+}
