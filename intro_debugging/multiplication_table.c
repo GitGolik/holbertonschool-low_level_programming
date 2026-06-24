c964d43de32592ac92fc4817435389d16d52ee9a
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 13:07:21 +0200

    multiplication_table.c

---
+++ intro_debugging/multiplication_table.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * print_table - check the code
+ *
+ * main - check the code
+ *
+ * @size: character
+ *
+ * Return: Always 0 (Success)
+ */
+void print_table(int size)
+{
+	int row, col;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			printf("%d ", row * col);
+			col++;
+		}
+		printf("\n");
+	}
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+	print_table(4);
+	return 0;
+}
