b5ff94c6fd6b6662ce8d5c71a5518b7cb03c204b
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 19:44:49 +0200

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - will print what we want in diagonal
+ *
+ * @n: character
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n != 0)
+	{
+		for (a = 0; a < n; ++a)
+		{
+			for (b = a; b < n; ++b)
+			{
+				_putchar(' ');
+			}
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
