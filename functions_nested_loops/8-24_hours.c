9bfdc78ccf6bd74808621a6bd06ecbe355a38309
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 09:40:24 +0200

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,16 +13,16 @@ void jack_bauer(void)
 	{
 		d = a / 10;
 		e = a % 10;
-		for (b = 0; b = < 60; ++b)
+		for (b = 0; b < 60; ++b)
 		{
 			f = b / 10;
 			g = b % 10;
 			_putchar('0' + d);
 			_putchar('0' + e);
-			_putchar(:);
+			_putchar(':');
 			_putchar('0' + f);
 			_putchar('0' + g);
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
