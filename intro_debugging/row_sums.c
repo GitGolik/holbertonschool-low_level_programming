c1fb95eb3f141ab7f04d5e6c458f45a53fc345c6
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 13:26:36 +0200

    row_sums.c

---
+++ intro_debugging/row_sums.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * row_sum - check the code
+ *
+ * total_sum - check the code
+ *
+ * main - check the code
+ *
+ * @row: character
+ *
+ * @cols: character
+ *
+ * Return: Always 0(Success)
+ */
+int row_sum(int row, int cols)
+{
+	int c;
+	int sum = 0;
+
+	for (c = 1; c <= cols; c++)
+		sum += row * c;
+
+	return sum;
+}
+
+int total_sum(int rows, int cols)
+{
+	int r;
+	int total = 0;
+	int current = 0;
+
+	for (r = 1; r <= rows; r++)
+	{
+		current += row_sum(r, cols);
+		total += current;
+	}
+
+	return total;
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+	printf("%d\n", total_sum(3, 3));
+	return 0;
+}
