4ced7069e776bd69b6cfe342d8765c770af42250
GitGolik <goliktheo.pro@gmail.com>
2026-06-24 11:14:30 +0200

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,13 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	while (n != 98)
 	{
-		for (n = n; n <= 98; ++n)
-		{
-			_putchar(n);
-		}
+		printf("%i, ", n);
+		if (n > 98)
+			--n;
+		else
+			++n;
 	}
-	else if (n > 98)
-	{
-		for (n = n; n >= 98; --n)
-		{
-			_putchar(n);
-		}
-	}
-	else
-	{
-		_putchar("98");
-	}
-	_putchar('\n');
+	printf("98\n");
 }
