46884e2adfb583c72b7229094d6b8e06f9e5bfd6
GitGolik <goliktheo.pro@gmail.com>
2026-06-30 16:16:08 +0200

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of the string
+ * @str: character
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int temp = 0;
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		for (temp = (length / 2); temp <= length; temp++)
+		{
+			_putchar(str[temp]);
+		}
+	}
+	_putchar('\n');
+}
