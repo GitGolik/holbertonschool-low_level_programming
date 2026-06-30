e37a36f706d5f81755789723acf807dce9c9b74f
GitGolik <goliktheo.pro@gmail.com>
2026-06-30 15:37:23 +0200

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: character
+ *
+ * Return: not wanted
+ */
+void rev_string(char *s)
+{
+	int start = 0;
+	int end = 0;
+	char tmp;
+
+	while (s[end] != '\0')
+		end++;
+	end--;
+
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+		start++;
+		end--;
+	}
+}
