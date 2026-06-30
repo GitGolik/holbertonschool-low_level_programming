8fe4d28cec994851ba33767b3143bd46b1e99e8c
GitGolik <goliktheo.pro@gmail.com>
2026-06-30 15:01:06 +0200

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -12,11 +12,15 @@ void _puts(char *str)
 	int a;
 
 	a = 0;
-	while (*(str + a) != '\0')
+	while (a >= 0)
 	{
-		_putchar(str);
+		if (str[a] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[a]);
 		a++;
 	}
-	_putchar('\n')
 }
 
