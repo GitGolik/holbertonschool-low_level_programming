350978e44479aa79639bef858f42bb97aabe112b
GitGolik <goliktheo.pro@gmail.com>
2026-06-30 14:19:02 +0200

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - let you swap the two pointer int
+ *
+ * @a: character
+ *
+ * @b: character
+ *
+ * Return: Always 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = temp;
+	*a = *b;
+	*b = temp;
+}
