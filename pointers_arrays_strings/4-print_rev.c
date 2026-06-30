f9d5289c161a0ab4f8bbeb2ae6dc646f0fce3b0c
GitGolik <goliktheo.pro@gmail.com>
2026-06-30 15:09:18 +0200

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print the string in reverse
+ *
+ * @s: character
+ *
+ * Return: not needed
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+	
+	while (a >= 0)
+	{
+		if (s[a] == '\0')
+			break;
+		a++;
+	}
+	for (a--;  a >= 0; a--)
+		_putchar(s[a]);
+	_putchar('\n');
+}
