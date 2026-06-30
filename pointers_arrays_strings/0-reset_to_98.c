6024e559aaf81b54d9c9628d66e3a16bbb532950
GitGolik <goliktheo.pro@gmail.com>
2026-06-30 13:33:44 +0200

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -9,9 +9,12 @@
  */
 void reset_to_98(int *n)
 {
-	*n = 402;
-	_putchar("n=%d\n", *n);
+	int n;
+	n = 98;
+	*n = &n;
+	n = 402;
+	_putchar("n=%d\n", n);
 	reset_to_98(&n);
-	_putchar("n=%d\n", *n);
+	_putchar("n=%d\n", n);
 	return (0);
 }
