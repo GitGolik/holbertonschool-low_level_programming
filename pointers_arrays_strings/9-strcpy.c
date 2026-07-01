affa93a56354bea05bc3d1645413ceac40bee0ce
GitGolik <goliktheo.pro@gmail.com>
2026-07-01 10:29:33 +0200

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - check the code
+ * @dest: character
+ * @src: character
+ * Return: always 0 (Success)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (a >= 0)
+	{
+		*(dest+a) = *(src + a);
+		if (src + a) == '\0'
+			break;
+		a++;
+	}
+	return (dest);
+}
