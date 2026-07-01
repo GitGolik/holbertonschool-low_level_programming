d59e18073445b79424c3d351b1ff0e06610ada48
GitGolik <goliktheo.pro@gmail.com>
2026-07-01 13:07:41 +0200

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strncpy - check the code
+ * @dest: character
+ * @src: character
+ * @n: character
+ * Return: 0 success
+ */
+char *_strncpy(char *dest, char *src , int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
+	for ( ; a < n; a++)
+		dest[a] = '\0';
+	return (dest);
+}
