9882c8401615667936ee32ec1803203017c31884
GitGolik <goliktheo.pro@gmail.com>
2026-07-01 15:33:02 +0200

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *string_toupper - check the code
+ * Return: 0 success
+ */
+void *string_toupper(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		if (s[a] >= 'a' && s[a] <= 'z')
+			s[a] -= 32;
+		a++;
+	}
+}
