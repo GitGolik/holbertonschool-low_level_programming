c756feb8be5463fc941ac4cc72602881906dc3bf
GitGolik <goliktheo.pro@gmail.com>
2026-07-01 15:07:14 +0200

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: character
+ * @n: character
+ * Return: no return
+ */
+void reverse_array(int *a, int n);
+{
+	int start = 0;
+	int end = n - 1;
+	int temp;
+
+	while (start < end)
+	{
+		temp = *(a + start);
+		*(a + start) = *(a + end);
+		*(a + end) = temp;
+		start++;
+		end--;
+	}
+}
