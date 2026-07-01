cff6943a9a2d09f6074086fbd4140326445076fe
GitGolik <goliktheo.pro@gmail.com>
2026-07-01 16:14:19 +0200

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ * @str: character
+ * Return: str to return
+ */
+char *cap_string(char *str)
+{
+	int  i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	if (str[0] >= 'a' && str[0] <= 'z') //capitalize first character if it's lowercase
+		str[0] -= 32;
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i-1] == sep[j])
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+				break;
+			}
+		}
+	}
+	return (str);
+}
