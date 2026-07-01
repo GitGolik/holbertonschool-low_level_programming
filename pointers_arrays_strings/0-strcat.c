e2ef393a130c34a2d3b93eb5207e7d11b15c9b6b
GitGolik <goliktheo.pro@gmail.com>
2026-07-01 11:35:42 +0200

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcat - check the code
+ * @dest: character
+ * @src: character
+ * Return: dest pointer
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(srx + count) == '\0')
+			break;
+		count++;
+	}
+	return (dest);
+}
