1718178dd67a807e326a6e09756a08b75bb4c7f4
GitGolik <goliktheo.pro@gmail.com>
2026-07-02 16:18:25 +0200

    calculator.c

---
+++ simple-calculator/calculator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ *
+ */
+int main(void)
+{
+	int start = 1;
+	int mynum;
+	int add;
+	int substract;
+	int multiply;
+	int divide;
+	int quit = 0;
+
+	printf("Welcome , would you like to start the calculator with 1 ou quit with 0 ?\n");
+		scanf("%d", &mynum);
+
+	if (mynum = 1)
+	{
+	}
+	else
+		return(0);
+}
