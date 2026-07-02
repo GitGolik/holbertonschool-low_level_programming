1a51c820ddd55165ef671a2c43fcf4fb4e6d7653
GitGolik <goliktheo.pro@gmail.com>
2026-07-02 11:29:55 +0200

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: the char we are searching
+ * Return: s return the result
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; ; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		if (s[i]=='\0')
+			return NULL;
+	}
+}
