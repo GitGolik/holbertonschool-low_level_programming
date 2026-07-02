f53bd9b9b4b8bdbf507a22d13a8cbd7b05757c99
GitGolik <goliktheo.pro@gmail.com>
2026-07-02 16:43:05 +0200

    calculator.c

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -6,20 +6,44 @@
  */
 int main(void)
 {
-	int start = 1;
+	int start;
 	int mynum;
+	int mynum2;
+	int sign;
 	int add;
 	int substract;
 	int multiply;
 	int divide;
-	int quit = 0;
 
 	printf("Welcome , would you like to start the calculator with 1 ou quit with 0 ?\n");
-		scanf("%d", &mynum);
+		scanf("%d", &start);
 
-	if (mynum = 1)
+	if (start == 1)
 	{
+		printf("Good , what's the calcul that you want to do :\n");
+		scanf("%d %d %d", &mynum, &sign, &mynum2);
+
+		if (sign == '+')
+		{
+			add = mynum + mynum2;
+			printf("The result is : %d", add);
+		}
+		else if (sign == '-')
+		{
+			substract = mynum - mynum2;
+			printf("The result is : %d", substract);
+		}
+		else if (sign == '/')
+		{
+			divide = mynum / mynum2;
+			printf("The result is : %d", divide);
+		}
+		else if (sign == '*')
+		{
+			multiply = mynum * mynum2;
+			printf("The result is : %d", multiply);
+		}
 	}
 	else
-		return(0);
+		return (0);
 }
