5756a74be24a2a9540266e44dda1e6aba8f14533
GitGolik <goliktheo.pro@gmail.com>
2026-07-02 11:13:51 +0200

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copy the src array to a dest 
+ * @dest: pointer of the destination of the src
+ * @src: pointer of the source of the array to copy in dest
+ * @n: number of octe to copy in from src to dest
+ * Return: dest to return
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i=0; i<n; i++)
+		dest[i] = src[i];
+	return dest;
+}
