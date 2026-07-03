cfac0d3bee951266a4872755d5bd5d5946e673ce
GitGolik <goliktheo.pro@gmail.com>
2026-07-03 11:24:57 +0200

    calculator.c

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -7,12 +7,22 @@
 int main(void)
 {
 	int choice;
+	float mynum, mynum2, result;
 
 	printf("Simple calculator: Choose your calcul\n 1) Add\n 2) Subtract\n 3) Multiply\n 4) Divide\n 0) quit\n");
 	scanf("%d", &choice);
 
 	while (choice != 0)
 	{
+		if (choice == 1)
+		{
+			printf("Choice: 1 , enter your number :\nA : ");
+			scanf("%f", &mynum);
+			printf("B : ");
+			scanf("%f", &mynum2);
+			result = mynum + mynum2;
+			printf("Result: %.1f\n", result);
+		}
 		if (choice > 4)
 		{
 			printf("Invalid choice\n");
