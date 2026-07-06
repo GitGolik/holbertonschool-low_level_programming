3eeda5af9a119499c783603369852a96d1c33dfa
GitGolik <goliktheo.pro@gmail.com>
2026-07-06 19:32:18 +0200

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return a pointer to a 2 dimensionnal array int
+ * @width: width of the array
+ * @height: height of the array
+ * Return: return result pointer or NULL if zero or negative
+ */
+int **alloc_grid(int width, int height)
+{
+	int h, w, **grid;
+
+	if (width <= 0)
+		return (NULL);
+	if (height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	h = 0;
+	while (h < height)
+	{
+		grid[h] = malloc(sizeof(int) * width);
+		if (grid[h] == NULL)
+		{
+			while (h > 0)
+			{
+				h--;
+				free(grid[h]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		h++;
+	}
+
+	h + 0;
+	while (h < height)
+	{
+		w = 0;
+		while (w < width)
+		{
+			grid[h][w] = 0;
+			w++;
+		}
+		h++;
+	}
+	return (grid);
+}
