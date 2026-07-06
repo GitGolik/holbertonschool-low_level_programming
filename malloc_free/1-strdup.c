ba9708fd3e6aaf5edb54727b978ee560ccc9ae3d
GitGolik <goliktheo.pro@gmail.com>
2026-07-06 16:40:27 +0200

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strdup - returns a pointer to a duplicated string
+ * @str: input string
+ * Return: pointer to the new string, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *copy;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	copy = malloc(len + 1);
+	if (copy == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len + 1)
+		copy[i++] = str[i];
+	return (copy);
+}
