abd1ea37445d2ea4f540aa0fb62c44098feec7f0
GitGolik <goliktheo.pro@gmail.com>
2026-07-06 17:04:25 +0200

    2-str_concat.c

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to new string, or NULL if it fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, len1, len2;
+	char *concat;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = 0;
+	len2 = 0;
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+	concat = malloc(len1 + len2 + 1);
+	if (concat == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	for (i = 0; i < len2; i++)
+		concat[len1 + i] = s2[i];
+	concat[len1 + len2] = '\0';
+	return (concat);
+}
