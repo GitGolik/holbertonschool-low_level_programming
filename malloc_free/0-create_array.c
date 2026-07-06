b3b9fcaa1254d134fcc8343255b946e9bd149d59
GitGolik <goliktheo.pro@gmail.com>
2026-07-06 16:22:25 +0200

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of char and initialize it after with specified char
+ * @size: unsigned integer
+ * @c: character
+ * Return: return result of char c
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
+	return (array);
+}
