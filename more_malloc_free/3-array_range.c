f35d475798e5a968994b64cbce1cbd0eb149c43c
GitGolik <goliktheo.pro@gmail.com>
2026-07-07 12:49:20 +0200

    3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,13 +11,14 @@ int *array_range(int min, int max)
 {
 	int *array;
 	unsigned int size, i;
+
 	if (min > max)
 		return (NULL);
 
 	size = max - min + 1;
 
 	array = malloc(sizeof(int) * size);
-	if (array==NULL)
+	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = min + 1;
