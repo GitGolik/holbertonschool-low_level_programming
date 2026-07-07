4a4db684ea14708c5222b66f5409d1fff7477881
GitGolik <goliktheo.pro@gmail.com>
2026-07-07 11:32:39 +0200

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, initialized to zero
+ * @nmemb: number of elements
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory, or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int total, i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	total = nmemb * size;
+
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < total; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
