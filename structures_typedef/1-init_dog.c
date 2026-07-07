b12f15bee86cae41484d84186fe0fbbe2e2be56f
GitGolik <goliktheo.pro@gmail.com>
2026-07-07 16:16:32 +0200

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,11 +7,13 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- * Description: This function initializes a struct dog variable with the provided name, age, and owner.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+    if (d)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
 }
