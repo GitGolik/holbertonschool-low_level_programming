fb6baa640f531aafff02c70c390523b97d0fa54e
GitGolik <goliktheo.pro@gmail.com>
2026-07-07 18:42:13 +0200

    4-new_dog.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+typedef struct dog dog_t;
+
 /**
  * struct dog - a structure that defines a dog
  * @name: name of the dog
@@ -18,9 +20,9 @@ char *owner;
 /**
  * dog_t - typedef of struct dog
  */
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
