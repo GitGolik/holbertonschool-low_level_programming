3ba3d2582bacc0a5f0a7d113625978ca59d13dbd
GitGolik <goliktheo.pro@gmail.com>
2026-07-07 18:39:31 +0200

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,8 +7,7 @@
  * @age: age of the dog.
  * @owner: owner of the dog.
  *
- * Return: struct dog.
- * if fails, returns NULL.
+ * Return: struct dog.if fails, returns NULL.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
