3c1ba623eaeb2436583d25a8f407bd68d978b4c2
GitGolik <goliktheo.pro@gmail.com>
2026-07-07 11:18:01 +0200

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,14 +26,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len2] != '\0')
 		len2++;
 
-	if (n <= len2)
+	if (n >= len2)
 		n = len2;
 
 	result = malloc(sizeof(char) * (len1 + n + 1));
 	if (result == NULL)
 		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
+
 	for (j = 0; j < n; j++)
 		result[i + j] = s2[j];
 
