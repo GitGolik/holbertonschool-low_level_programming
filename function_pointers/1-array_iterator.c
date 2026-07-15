4651d3edfe0309dd952a190394096cf4f18dc647
GitGolik <goliktheo.pro@gmail.com>
2026-07-15 12:02:33 +0200

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function on each element of an array
+ * @array: array to iterate
+ * @size: number of elements in the array
+ * @action; pointer to the function to use
+ * Return: no return
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    size_t i;
+    
+    if (array == NULL || action == NULL)
+        return;
+    for (i = 0; i < size; i++)
+        action(array[i]);
+}
