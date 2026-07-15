db7c50ccc8c76e78fc54d598c083037718021d0d
GitGolik <goliktheo.pro@gmail.com>
2026-07-15 11:50:11 +0200

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,12 +1,12 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+#include <stdlib.h>
 
 /**
  * definition of function pointers that will be used
  */
-#include <stdlib.h>
 
-int _putchar(char c);
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 
 #endif
