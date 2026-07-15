d03da73f0dd63136207a266efb6619c91148be5e
GitGolik <goliktheo.pro@gmail.com>
2026-07-15 11:52:40 +0200

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 
 /**
- * definition of function pointers that will be used
  * _putchar - like putchar
  * print_name - to print the name
+ * @c: character
+ * @name: name to print
  */
 
-int _putchar(char);
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 #endif
