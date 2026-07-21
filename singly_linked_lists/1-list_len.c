2d7941a019586dc76c75dea34d1f9d8c6bc64b12
GitGolik <goliktheo.pro@gmail.com>
2026-07-21 16:12:29 +0200

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list_t list
+ * @h: pointer to the head of the list
+ * Return: the number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+    const list_t *current = h;
+    size_t count = 0;
+
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+    return (count);
+}
