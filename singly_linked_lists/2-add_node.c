ee2d071e26f4670b0534e87adaf4a5b0770face3
GitGolik <goliktheo.pro@gmail.com>
2026-07-21 16:27:40 +0200

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,29 +10,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new;
-    unsigned int len = 0;
+list_t *new;
+unsigned int len = 0;
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+return (NULL);
 
-    while (str[len] != '\0')
-        len++;
+while (str[len] != '\0')
+len++;
     
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
+new = malloc(sizeof(list_t));
+if (new == NULL)
+return (NULL);
 
-    new->str = strdup(str);
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
+new->str = strdup(str);
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
 
-    new->len = len;
-    new->next = *head;
-    *head = new;
+new->len = len;
+new->next = *head;
+*head = new;
 
-    return (new);
+return (new);
 }
