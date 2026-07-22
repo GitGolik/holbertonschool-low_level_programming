359dd7c6a2360353b0e526505ecc3ff6fd6c48b0
GitGolik <goliktheo.pro@gmail.com>
2026-07-22 11:54:02 +0200

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - recursion of x raised to the power of y
+ * @x: number to recursive
+ * @y: power to add on x
+ * Return: 1 success , -1 error
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+    {
+        return (-1);
+    }
+    if (x == 0)
+    {
+        return (1);
+    }
+    return ((x * _pow_recursion(x - 1, y)));
+}
