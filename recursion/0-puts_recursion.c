4af137cb9f7c8d24491b3387cb22df32f0bb2dca
GitGolik <goliktheo.pro@gmail.com>
2026-07-22 11:05:27 +0200

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _puts_recursion - a puts with recursion of char s
+ * _puts_recursion - prints a string and a new line
  * @s: string to print
  * Return: no return
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
-    
-    _putchar("%c", *s);
-    s++;
-    _puts_recursion(*s);
-
-    _putchar('\n');
+    if (*s != '\0')
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else
+    {
+        _putchar('\n');
+    }
 }
