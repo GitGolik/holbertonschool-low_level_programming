e51bc4a06df4181415d5bbfff182b1f15d30b6fa
GitGolik <goliktheo.pro@gmail.com>
2026-07-22 11:41:23 +0200

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of given number
+ * @n: number in factorial
+ * Return: 1 succes , -1 error
+ */
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return (1);
+    }
+    return (n * factorial(n - 1))
+}
