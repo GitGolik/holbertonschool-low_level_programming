45f263489fd5da8ecb276d903f3207a8392c9f7b
GitGolik <goliktheo.pro@gmail.com>
2026-07-22 12:14:35 +0200

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _sqrt_helper - recursively finds natural square root of a number
+ * @n: number to find the square root of i
+ * @i: current candidate
+ * Return: natural square root of n, or -1 if none
+ */
+int _sqrt_helper(int n, int i)
+{
+    if (i * i == n)
+        return (i);
+    if (i * i > n)
+        return (-1)
+    return (_sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - print natural square root of a number
+ * @n: number to find the square root of 
+ * Return: natural square root of n, or -1 if none
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+    return (_sqrt_helper(n, 0));
+}
