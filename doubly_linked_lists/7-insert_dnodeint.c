c3a180305e91af30c1b9ca17845929fb1ecae84e
GitGolik <goliktheo.pro@gmail.com>
2026-07-28 15:29:23 +0200

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -5,6 +5,7 @@
  * @h: pointer to the head of the list
  * @idx: index where the new node should be added, starting at 0
  * @n: value to store in the new node
+ *
  * Return  the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
