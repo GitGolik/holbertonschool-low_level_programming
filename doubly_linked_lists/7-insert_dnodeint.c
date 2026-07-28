aca341ae74801fb113479a9819f562c9a750c2de
GitGolik <goliktheo.pro@gmail.com>
2026-07-28 15:28:22 +0200

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (prev_node == NULL)
 		return (NULL);
 
-	if (prev_node->next = NULL)
+	if (prev_node->next == NULL)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
