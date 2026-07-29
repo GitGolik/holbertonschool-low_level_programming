87dd489ce0ad4d5995618d14bfb1541eb18d11c1
GitGolik <goliktheo.pro@gmail.com>
2026-07-29 15:31:13 +0200

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,6 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *current;
 	char *value_copy;
+
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 	value_copy = strdup(value);
