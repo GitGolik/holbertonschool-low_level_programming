d072484523a7410808185a232b328d4e999c2305
GitGolik <goliktheo.pro@gmail.com>
2026-07-29 15:57:00 +0200

    4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve the value associated with a key
+ * @ht: hash table
+ * @key: key to look for
+ * Return value string, or NULL if not found or on error
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int index;
+    hash_node_t *node;
+
+    if (ht == NULL || key == NULL || key[0] == '\0')
+        return (NULL);
+
+    index = key_index((const unsigned char *)key, ht->size);
+    node = ht->array[index];
+
+    while (node)
+    {
+        if (strcmp(node->key, key) == 0)
+            return (node->value);
+        node = node->next;
+    }
+
+    return (NULL);
+}
